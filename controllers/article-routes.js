79a6259daefc1b0e27cc24a410c8d7d2aba1d8d4
Valerie Fragier <valfragier@gmail.com>
2018-12-13 21:09:12 -0500

    updated:

--- controllers/article-routes.js
+++ controllers/article-routes.js
@@ -9,6 +9,41 @@ var axios = require("axios");
 
 // ============= ROUTES FOR HOME PAGE =============//
 
+// First, tell the console what server2.js is doing
+console.log("\n******************************************\n" +
+            "Grabbing every article headline and link\n" +
+            "from the LIVEScience website:" +
+            "\n******************************************\n");
+
+// Making a request via axios for `nhl.com`'s homepage
+axios.get("https://www.livescience.com/space?type=article").then(function(response) {
+
+  // Load the body of the HTML into cheerio
+  var $ = cheerio.load(response.data);
+
+  // Empty array to save our scraped data
+  var results = [];
+
+  // With cheerio, find each h4-tag with the class "headline-link" and loop through the results
+  $("h2").each(function(i, element) {
+
+    // Save the text of the h4-tag as "title"
+    var title = $(element).text();
+
+    // Find the h4 tag's parent a-tag, and save it's href value as "link"
+    var link = $(element).children().attr("href");
+
+    // Make an object with data we scraped for this h4 and push it to the results array
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  // After looping through each h4.headline-link, log the results
+  console.log(results);
+});
+
 // Scrape data from NPR website and save to mongodb
 router.get("/scrape", function(req, res) {
   
@@ -21,13 +56,6 @@ router.get("/scrape", function(req, res) {
 
       // Save an empty result object
       var result = {};
-
-      // Get the title and description of every article, and save them as properties of the result object
-      // result.title saves entire <a> tag as it appears on NPR website
-    //   var title = $(element).text();
-
-    // // Find the h4 tag's parent a-tag, and save it's href value as "link"
-    // var link = $(element).children().attr("href");
       
       
       result.title = $(element).text();
